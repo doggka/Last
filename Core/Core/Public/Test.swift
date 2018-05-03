8057b195fb4badbf312259212d6f9b80d646f4be
Милованов Андрей <milovanov@skbkontur.ru>
2018-05-03 19:02:52 +0500

    init

---
+++ Core/Core/Public/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Core
+//
+//  Created by Милованов Андрей on 03/05/2018.
+//  Copyright © 2018 СКБ Контур. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass {
+  public static func test() {
+    print("TEST MODULE")
+  }
+}
---
+++ LastApp/Pods/Target Support Files/Pods-LastApp/Pods-LastApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LastApp : NSObject
+@end
+@implementation PodsDummy_Pods_LastApp
+@end
